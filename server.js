c266d0f25dc94dedd1545e7342a25414b7b70976
james <50460460+alexacallmebaka@users.noreply.github.com>
2022-04-10 01:57:52 -0500

    the ssl dream is dead

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require("path")
 const app = express()
-const port = 443
+const port = 80
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, './index.html'))
